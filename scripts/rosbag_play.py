2a6f8c40622bc56c320a2d04f18818147c77d38b
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2018-09-02 19:35:37 +0900

    First commit.

---
+++ scripts/rosbag_play.py
@@ -0,0 +1,67 @@
+import rosbag
+import rospy
+from sensor_msgs.msg import Image
+from time import sleep
+import argparse
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help='path for d435 rosbag', required=True)
+    args = parser.parse_args()
+    return args
+
+
+class D435Publisher:
+    def __init__(self):
+        self.image_topic = '/device_0/sensor_1/Color_0/image/data'
+        self.depth_topic = '/device_0/sensor_0/Depth_0/image/data'
+        self.infrared_topic = '/device_0/sensor_0/Infrared_1/image/data'
+
+        self.image_pub = rospy.Publisher(self.image_topic, Image, queue_size=10)
+        self.depth_pub = rospy.Publisher(self.depth_topic, Image, queue_size=10)
+        self.infrared_pub = rospy.Publisher(self.infrared_topic, Image, queue_size=10)
+        self.bridge = CvBridge()
+
+    def read(self, path):
+        r = rospy.Rate(10)
+        index = 10
+        with rosbag.Bag(path) as bag:
+            for topic, msg, t in bag.read_messages(
+                    topics=[self.image_topic,
+                            self.depth_topic,
+                            self.infrared_topic]):
+                if topic == self.image_topic:
+                    self.image_pub.publish(msg)
+
+                    if index % 10 != 0:
+                        index += 1
+                        continue
+                    try:
+                        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                    except CvBridgeError as e:
+                        print(e)
+
+                    cv2.imwrite("../data/{:06d}.png".format(index), cv_image)
+
+                    index += 1
+                if topic == self.depth_topic:
+                    self.depth_pub.publish(msg)
+                if topic == self.infrared_topic:
+                    self.infrared_pub.publish(msg)
+                rospy.sleep(0.1)
+
+
+def main():
+    options = parseArguments()
+    
+    rospy.init_node("d435_publisher" ,anonymous=True, disable_signals=True)
+    publisher = D435Publisher()
+
+    publisher.read(options.path)
+
+
+if __name__ == "__main__":
+    main()
