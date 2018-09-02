54dc8db71aee994584a6bc6dd554dac96ebca277
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2018-09-02 19:58:43 +0900

    Add dump image setting.

--- scripts/rosbag_play.py
+++ scripts/rosbag_play.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
 import rosbag
 import rospy
 from sensor_msgs.msg import Image
@@ -10,6 +14,8 @@ from cv_bridge import CvBridge, CvBridgeError
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', help='path for d435 rosbag', required=True)
+    parser.add_argument('--dump', default=False, action='store_true',
+                        help='dump image while play rosbag.')
     args = parser.parse_args()
     return args
 
@@ -20,14 +26,18 @@ class D435Publisher:
         self.depth_topic = '/device_0/sensor_0/Depth_0/image/data'
         self.infrared_topic = '/device_0/sensor_0/Infrared_1/image/data'
 
-        self.image_pub = rospy.Publisher(self.image_topic, Image, queue_size=10)
-        self.depth_pub = rospy.Publisher(self.depth_topic, Image, queue_size=10)
-        self.infrared_pub = rospy.Publisher(self.infrared_topic, Image, queue_size=10)
+        self.image_pub = rospy.Publisher(
+            self.image_topic, Image, queue_size=10)
+        self.depth_pub = rospy.Publisher(
+            self.depth_topic, Image, queue_size=10)
+        self.infrared_pub = rospy.Publisher(
+            self.infrared_topic, Image, queue_size=10)
         self.bridge = CvBridge()
 
-    def read(self, path):
+    def read(self, path, dump):
         r = rospy.Rate(10)
-        index = 10
+        directory = os.path.dirname(path)
+        index = 0
         with rosbag.Bag(path) as bag:
             for topic, msg, t in bag.read_messages(
                     topics=[self.image_topic,
@@ -36,16 +46,13 @@ class D435Publisher:
                 if topic == self.image_topic:
                     self.image_pub.publish(msg)
 
-                    if index % 10 != 0:
-                        index += 1
-                        continue
-                    try:
-                        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-                    except CvBridgeError as e:
-                        print(e)
-
-                    cv2.imwrite("../data/{:06d}.png".format(index), cv_image)
-
+                    if dump:
+                        try:
+                            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                        except CvBridgeError as e:
+                            print(e)
+                        cv2.imwrite(
+                            os.path.join(directory, "{:06d}.png").format(index), cv_image)
                     index += 1
                 if topic == self.depth_topic:
                     self.depth_pub.publish(msg)
@@ -56,11 +63,11 @@ class D435Publisher:
 
 def main():
     options = parseArguments()
-    
-    rospy.init_node("d435_publisher" ,anonymous=True, disable_signals=True)
+
+    rospy.init_node("d435_publisher", anonymous=True, disable_signals=True)
     publisher = D435Publisher()
 
-    publisher.read(options.path)
+    publisher.read(options.path, options.dump)
 
 
 if __name__ == "__main__":
